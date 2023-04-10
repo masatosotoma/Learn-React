ff55fffdf05db90195290495bd54fcf1c1be48aa
masatosotoma <masatosotoma@gmail.com>
2023-04-10 19:39:33 -0400

    Adding click function to the squares

--- Get-started/tutorial-tick-tack-toe/src/App.js
+++ Get-started/tutorial-tick-tack-toe/src/App.js
@@ -1,32 +1,38 @@
 import { useState } from "react";
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square">
-      {value}
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
-  const [squares, setSquares] = setState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handlClick() {
+    const nextsquare = squares.spice;
+    nextsquare[0] = "X";
+    setSquares(nextsquare);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onClick={handlClick} />
+        <Square value={squares[1]} onClick={handlClick} />
+        <Square value={squares[2]} onClick={handlClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onClick={handlClick} />
+        <Square value={squares[4]} onClick={handlClick} />
+        <Square value={squares[5]} onClick={handlClick} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onClick={handlClick} />
+        <Square value={squares[7]} onClick={handlClick} />
+        <Square value={squares[8]} onClick={handlClick} />
       </div>
     </>
   );
