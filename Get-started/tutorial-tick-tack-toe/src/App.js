6edf960bd26ae15250dc4f80e450de6dbef21185
masatosotoma <masatosotoma@gmail.com>
2023-04-18 23:24:57 -0400

    Added jumpTo func and moves var for description

--- Get-started/tutorial-tick-tack-toe/src/App.js
+++ Get-started/tutorial-tick-tack-toe/src/App.js
@@ -62,13 +62,29 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {}
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = "Go to move # " + move;
+    } else {
+      description = "Go to game start.";
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquare} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
